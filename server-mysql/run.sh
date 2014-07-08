e098980eb46e69a4aa0b00e4c3f17ed6f4328d0a
Daniel Graña <dangra@gmail.com>
2014-07-08 03:05:56 -0300

    add zabbix-server-mysql

---
+++ server-mysql/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pidfile=/var/run/zabbix/zabbix_server.pid
+piddir=$(dirname $pidfile)
+mkdir -p $piddir
+chown -R zabbix:zabbix $piddir
+/usr/sbin/zabbix_server -c /etc/zabbix/zabbix_server.conf
+trap "pkill -F $pidfile" INT TERM EXIT
+tail -f /var/log/zabbix/zabbix_server.log &
+wait $(cat $pidfile)
