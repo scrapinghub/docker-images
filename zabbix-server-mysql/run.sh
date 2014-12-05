0eb9819e490db1f27fd61a48dc2a04beb89fb834
Luar Roji <luar@roji.net>
2014-12-05 17:40:37 -0200

    Fixed the loop using tail --pid

--- zabbix-server-mysql/run.sh
+++ zabbix-server-mysql/run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
 pidfile=/var/run/zabbix/zabbix_server.pid
 piddir=$(dirname $pidfile)
+
 mkdir -p $piddir
 chown -R zabbix:zabbix $piddir
+
 /usr/sbin/zabbix_server -c /etc/zabbix/zabbix_server.conf
+
 trap "pkill -F $pidfile" INT TERM EXIT
-tail -f /var/log/zabbix/zabbix_server.log &
-wait $(cat $pidfile)
+
+tail -f /var/log/zabbix/zabbix_server.log --pid=$(cat $pidfile)
