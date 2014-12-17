f91ad49323417fd1c1abb51f11c13381ec72a9c6
Daniel Graña <dangra@gmail.com>
2014-12-17 17:23:25 -0200

    Add a way to link zabbix frontend config to mesos sandbox

--- zabbix-frontend-php/run.sh
+++ zabbix-frontend-php/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [[ $ZABBIX_CONF ]]; then
+  ln -sf "$ZABBIX_CONF" /etc/zabbix/web/zabbix.conf.php
+fi
 . /etc/default/apache2
 . /etc/apache2/envvars
 /usr/sbin/apache2 -D FOREGROUND
