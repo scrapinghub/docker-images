aa1d2a219c35790c3808e6f13daa98d9befb681b
Luar Roji <luar@scrapinghub.com>
2015-08-26 17:17:29 +0100

    Made apache conf. file configurable

--- zabbix-frontend-php/run.sh
+++ zabbix-frontend-php/run.sh
@@ -2,6 +2,9 @@
 if [[ $ZABBIX_CONF ]]; then
   ln -sf "$ZABBIX_CONF" /etc/zabbix/web/zabbix.conf.php
 fi
+if [[ $APACHE_CONF ]]; then
+  ln -sf "$APACHE_CONF" /etc/apache2/sites-enabled/
+fi
 . /etc/default/apache2
 . /etc/apache2/envvars
 /usr/sbin/apache2 -D FOREGROUND
