f268b9574fdabf1f748f18fe963198d609c46e32
Luar Roji <luar@scrapinghub.com>
2015-08-26 02:28:55 +0100

    Also made LOGFILE configurable

--- zabbix-server-mysql/run.sh
+++ zabbix-server-mysql/run.sh
@@ -3,6 +3,7 @@
 PIDDIR=${PIDDIR:-/var/run/zabbix}
 PIDFILE=${PIDFILE:-$PIDDIR/zabbix_server.pid}
 CFGFILE=${CFGFILE:-/etc/zabbix/zabbix_server.conf}
+LOGFILE=${LOGFILE:-/var/log/zabbix/zabbix_server.log}
 
 mkdir -p $PIDDIR
 chown -R zabbix:zabbix $PIDDIR
@@ -10,7 +11,7 @@ chown -R zabbix:zabbix $PIDDIR
 /usr/sbin/zabbix_server -c $CFGFILE
 
 sleep 5
-tail -f /var/log/zabbix/zabbix_server.log --pid=$(cat $PIDFILE)
+tail -f $LOGFILE --pid=$(cat $PIDFILE)
 
 # Provide some time in case something needs to be debugged
 sleep 30
