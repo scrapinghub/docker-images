bd731f1697b5d748251db880c46bee2f3e1b7a32
Luar Roji <luar@scrapinghub.com>
2015-08-26 18:18:22 +0100

    In case APACHE_CONF is set, remove default apache site config

--- zabbix-frontend-php/run.sh
+++ zabbix-frontend-php/run.sh
@@ -3,6 +3,7 @@ if [[ $ZABBIX_CONF ]]; then
   ln -sf "$ZABBIX_CONF" /etc/zabbix/web/zabbix.conf.php
 fi
 if [[ $APACHE_CONF ]]; then
+  rm -rf /etc/apache2/sites-enabled/*
   ln -sf "$APACHE_CONF" /etc/apache2/sites-enabled/
 fi
 . /etc/default/apache2
