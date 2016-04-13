89b666a23a28bf8aed95b764910f6993789530ef
Shuai Lin <linshuai2012@gmail.com>
2016-04-13 19:20:50 +0800

    Added a spark base image

---
+++ spark/spark-env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# This file is sourced when running various Spark programs.
+# Copy it as spark-env.sh and edit that to configure Spark for your site.
+
+export MESOS_NATIVE_JAVA_LIBRARY=/usr/local/lib/libmesos.so
