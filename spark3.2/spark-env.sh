f50b968033f08869ee334cc663cd1dcb6baa791a
Shuai Lin <linshuai2012@gmail.com>
2022-06-10 19:59:32 +0800

    Added image for spark3.2

---
+++ spark3.2/spark-env.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This file is sourced when running various Spark programs.
+# Copy it as spark-env.sh and edit that to configure Spark for your site.
+
+export MESOS_NATIVE_JAVA_LIBRARY=/usr/local/lib/libmesos.so
+
+if [[ -e /mnt/mesos/sandbox/spark-env.sh ]]; then
+    . /mnt/mesos/sandbox/spark-env.sh
+fi
